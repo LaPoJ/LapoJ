4b7d3e10946dd137c5fdbf2e2bcc27284b8132b3
LaPoj <2295140520@qq.com>
2019-03-07 16:00:56 +0800

    1

--- PHP/From/index.php
+++ PHP/From/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<title>用户登录</title>
+    <meta charset="UTF-8">
+    <title>用户登录</title>
 </head>
+
 <body>
-	<!--
+    <!--
 		1.必须有form标签
 		2.form必须指定 action 和 method
 			不设置 action 默认当前页面（必须设置，因为兼容）
@@ -13,21 +15,22 @@
 		3.表单元素（表单域--input）必须有 name (如果希望被提交的情况下)
 		4.表单中必须有一个提交按钮
 	 -->
-	<form action="formP.php" method="get">
-		<table border="1";>
-			<tr>
-<td>用户名</td>
-				<td><input type="text" name="username"></td>
-			</tr>
-			<tr>
-				<td>密码</td>
-				<td><input type="text" name="password"></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit" value="登录"></td>
-			</tr>
-		</table>	
-	</form>
+    <form action="formP.php" method="get">
+        <table border="1" ;>
+            <tr>
+                <td>用户名</td>
+                <td><input type="text" name="username"></td>
+            </tr>
+            <tr>
+                <td>密码</td>
+                <td><input type="text" name="password"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="登录"></td>
+            </tr>
+        </table>
+    </form>
 </body>
-</html>
+
+</html> 
