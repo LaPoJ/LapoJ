9ffb92319b0c4a3bfe889b694f9d221831def267
LP_ZX <2295140520@qq.com>
2019-04-22 18:47:14 +0800

    js main

--- project/landRover-B/js/main.js
+++ project/landRover-B/js/main.js
@@ -12,10 +12,22 @@
       }
   })
   // carousel
+  // slick 轮播
   $('.ui.hero.carousel').slick(
-  {
-    prevArrow: '<button class="ui prev bottom button"><i class="material-icons">chevron_left</i></button>',
-    nextArrow: '<button class="ui next bottom button"><i class="material-icons">chevron_right</i></button>'
-  }
+    {
+      prevArrow: '<button class="ui prev bottom button"><i class="material-icons">chevron_left</i></button>',
+      nextArrow: '<button class="ui next bottom button"><i class="material-icons">chevron_right</i></button>'
+    }
   )
+
+  // semantic.js
+  $('.ui.sidebar')
+    .sidebar('setting', 'dimPage', true)
+    .sidebar('attach events', '.ui.navbar .menu-icon', 'show')
+    .sidebar('attach events', '.ui.sidebar .close-icon', 'hidden')
+
+  // sidebar
+  $('.ui.navbar .menu')
+    .clone()
+    .appendTo('.ui.sidebar')
 })();
