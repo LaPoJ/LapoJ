4958213119d8458e6e65581915e9bc55f4a86fbc
LP_ZX <2295140520@qq.com>
2019-05-29 23:46:19 +0800

    menu

---
+++ Vue/vue -demo_0527/element/src/components/common/Slide.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="sidebar">
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      background-color="#eeeeee"
+      router
+    >
+      <el-menu-item index="/main">
+        <i class="el-icon-menu"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-setting"></i>
+          <span>用户管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="2-1">
+            <i class="el-icon-user-solid"></i>用户列表
+          </el-menu-item>
+          <el-menu-item index="2-2">
+            <i class="el-icon-s-unfold"></i>用户信息
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  naem: 'Sidebar',
+  data () {
+    return
+  }
+}
+</script>
+
+<style scoped>
+.sidebar{
+  display: block;
+  position: absolute;
+  width: 200px;
+  left: 0;
+  top: 70px;
+  bottom: 0;
+  background: #2d363f;
+}
+.sidebar > ul {
+  height: 100%;
+}
+</style>
