6bf761746748859e652c23be55878ac80cb1c9a8
LP_ZX <2295140520@qq.com>
2019-06-02 08:54:15 +0800

    user info

---
+++ Vue/vue -demo_0527/element/src/components/UsersInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="users-info">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersInfo',
+  data () {
+    return {
+      msg: '用户信息'
+    }
+  }
+}
+</script>
