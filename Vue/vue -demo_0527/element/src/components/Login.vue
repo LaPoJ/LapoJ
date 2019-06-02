5980ae0823f18c06f6ea43c09bb12eae67ff5c63
LP_ZX <2295140520@qq.com>
2019-06-02 08:53:55 +0800

    login

--- Vue/vue -demo_0527/element/src/components/Login.vue
+++ Vue/vue -demo_0527/element/src/components/Login.vue
@@ -4,9 +4,11 @@
       <p class="title">后台管理系统</p>
       <div class="msg mt20">
         <el-input v-model="user" placeholder="请输入内容"></el-input>
+        <span class="username-err">{{ userErr }}</span>
       </div>
       <div class="msg mt20">
-        <el-input v-model="password" placeholder="请输入内容"></el-input>
+        <el-input v-model="password" type="password" placeholder="请输入内容"></el-input>
+        <span class="username-err">{{ passErr }}</span>
       </div>
       <div class="btn mt20">
         <!-- <router-link to="hello"> -->
@@ -23,16 +25,35 @@ export default {
   data () {
     return {
       user: '',
-      password: ''
+      password: '',
+      userErr: '',
+      passErr: ''
     }
   },
   methods: {
     login () {
+      const userName = localStorage.getItem('userName')
+      const password = localStorage.getItem('password')
       if (this.user && this.password) {
-        localStorage.setItem('userName', this.user)
-        this.$router.push('/index') // 字符串跳转
-        // this.$router.push({ path: '/hello'}) //对象跳转
-        // this.$router.push({ name: 'HelloWorld', param: {userId: 1} })
+        if (this.user !== userName && this.password === password) {
+          this.userErr = '用户名错误'
+          this.passErr = ''
+        }
+        if (this.user === userName && this.password !== password) {
+          this.userErr = ''
+          this.passErr = '密码错误'
+        }
+        if (this.user !== userName && this.password !== password) {
+          this.userErr = '用户名错误'
+          this.passErr = '密码错误'
+        }
+        if (this.user === userName && this.password === password) {
+          localStorage.setItem('userName', this.user)
+          localStorage.setItem('password', this.password)
+          this.$router.push('/index') // 字符串跳转
+          // this.$router.push({ path: '/hello'}) //对象跳转
+          // this.$router.push({ name: 'HelloWorld', param: {userId: 1} })
+        }
       }
     }
   }
@@ -61,5 +82,9 @@ export default {
   font-size: 26px;
   font-weight: bold;
   color: #505458;
+}
+.username-err{
+  font-size: 12px;
+  color: red;
 }
 </style>
