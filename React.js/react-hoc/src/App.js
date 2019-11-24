c66496d18c374d962e1c774c35315be822bdb6d2
LP_ZX <2295140520@qq.com>
2019-11-24 14:43:34 +0800

    react hoc

--- React.js/react-hoc/src/App.js
+++ React.js/react-hoc/src/App.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import './App.css'
 
-// import WithLogin from './components/WithLogin'
+import WithLogin from './components/WithLogin'
 import LoginStatus from './components/LoginStatus'
 import ShopCart from './components/ShopCart'
 
-import ExampleMobx from './exampleMobx/index'
-import ExampleDeBounce from './ExampleDeBounce/index';
+// import ExampleMobx from './exampleMobx/index'
+// import ExampleDeBounce from './ExampleDeBounce/index'
 
-// const WithLoginStatus = WithLogin(LoginStatus)
-// const WithShopCart = WithLogin(ShopCart)
+const WithLoginStatus = WithLogin(LoginStatus)
+const WithShopCart = WithLogin(ShopCart)
 
 function App() {
   return (
     <React.Fragment>
-      <LoginStatus />
-      <ShopCart />
 
-      {/* <WithLoginStatus />
-      <WithShopCart a="1" b="2"/> */}
+      {/* <LoginStatus /> */}
 
-      <ExampleMobx />
 
-      <ExampleDeBounce />
+      {/* <ShopCart a={1} b={3} /> */}
+
+      {/* <WithLoginStatus /> */}
+      <WithShopCart a="1" b="2"/>
+
+      {/* <ExampleMobx />
+
+      <ExampleDeBounce /> */}
 
     </React.Fragment>
   );
--- React.js/react-hoc/src/ExampleDeBounce/index.js
+++ React.js/react-hoc/src/ExampleDeBounce/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+ import React from 'react'
 import { debounce, decDebounce, decArrowDebounce, decDisplayName } from './util';
 
 
